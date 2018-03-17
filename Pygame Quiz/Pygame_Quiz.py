715f80a7ab303c1400590a9a1e1c7bc9b3095043
Austin Lopez <30838351+austinlopez@users.noreply.github.com>
2018-03-17 10:39:57 -0700

    3/16 commit

--- Pygame Quiz/Pygame_Quiz.py
+++ Pygame Quiz/Pygame_Quiz.py
@@ -16,6 +16,10 @@ SOUND_PATH = ''
 IMAGE_DIR = '.\images'
 DATA_DIR = '.\data'
 BACKGROUND_COLOR = [211,211,255]#pygame.color.THECOLORS["grey"]
+ANSWER1_COLOR = []
+ANSWER2_COLOR = []
+ANSWER3_COLOR = []
+ANSWER4_COLOR = []
 
 BG_COLOR = [211,211,255]
 
@@ -59,8 +63,13 @@ def text_to_screen(screen, text, x, y, size = 50, color = (50, 50, 50), font_typ
 """
 
 class Block():
+    """
+        The actual block of the answer.
+        Consists of the function to draw the rectangle.
 
-    def __init__(self, name, color, x, y, width, height, outlineColor=[0]):
+    """
+
+    def __init__(self, name, x, y, width, height, color, outlineColor=[0]):
         self.name = name
         self.color = color
         #self.pos = pos
@@ -83,32 +92,52 @@ class Block():
             return True
 
 class Answers(pygame.sprite.Sprite):
-
+    """
+        The Object for each answer
+        Takes in:
+            the screen
+            the question (name)
+            the x, y, width, and height of the box for the question
+            the color and outline color of the box
+        Functions:
+            create the Block
+    """
     def __init__(self,screen, name, x, y, width, height, color,bgColor=[0]):
         self.answerName = name
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([width, height])
+        self.x = x; self.y = y; self.width = width; self.height = height
         self.image.fill(color)
+        self.color = color
+        self.bgColor = bgColor
         self.screen = screen
 
-        self.rect = Block(self.answerName, color,x,y, 300, 300,bgColor)#, [255,0,0])
+        self.createAnswerBlock(self.answerName)
+
+        #self.rect = Block(self.answerName, x,y, self.width, self.height,color,bgColor)#, [255,0,0])
 
     def createAnswerBlock(self,answers):
-        answer1 = Block('answer1',[255,128,64],10,300,250,100,[0,0,0])
-        answer2 = Block('answer1',[255,128,0],270,300,250,100,[0,0,0])
-        answer3 = Block('answer1',[128,64,64],10,410,250,100,[0,0,0])
-        answer4 = Block('answer1',[128,0,255],270,410,250,100,[0,0,0])
-        answer1.draw(self.screen)
-        answer2.draw(self.screen)
-        answer3.draw(self.screen)
-        answer4.draw(self.screen)
+        #Need to implement the text that goes into the block. 
+        self.rect = Block(self.answerName, self.x,self.y, self.width, self.height,self.color,self.bgColor)#, [255,0,0])
+        pass
 
-    def draw(self):
-        self.rect.draw(self.screen)
+    def draw(self, screen):
+        self.rect.draw(screen)
 
 
 class Question():
-
+    """
+        Start a question for the quiz
+        Takes in:
+            the screen being displayed to
+            the topic
+            the question
+            the answer choices
+            the correct answer
+        Functions:
+            create the aswer objects and store in a dictionary
+            check if the one clicked is correct
+    """
     def __init__(self, screen, topic, question, answers, correctAnswer):
         self.screen = screen
         self.topic = topic
@@ -121,29 +150,51 @@ class Question():
         #print(topic,question,answers,correctAnswer)
 
     def createAnswers(self):
-        answerNum = 0
+        answerNum = 1
         for answer in self.answers:
-            if answerNum == 0:
-                self.answerBlocks[answerNum] = Answers(self.screen, answer, 10,300,250,100, [255,128,64], [0,0,0])
             if answerNum == 1:
-                self.answerBlocks[answerNum] = Answers(self.screen, answer, 270,300,250,100, [255,128,0], [0,0,0])
+                self.answerBlocks[answerNum] = Answers(self.screen, answer, 10,300,380,130, [255,128,64], [0,0,0])
             if answerNum == 2:
-                self.answerBlocks[answerNum] = Answers(self.screen, answer, 10,410,250,100, [128,64,64], [0,0,0])
+                self.answerBlocks[answerNum] = Answers(self.screen, answer, 410,300,380,130, [255,128,0], [0,0,0])
             if answerNum == 3:
-                self.answerBlocks[answerNum] = Answers(self.screen, answer, 270,410,250,100,[128,0,255], [0,0,0])
+                self.answerBlocks[answerNum] = Answers(self.screen, answer, 10,450,380,130, [128,64,64], [0,0,0])
+            if answerNum == 4:
+                self.answerBlocks[answerNum] = Answers(self.screen, answer, 410,450,380,130,[128,0,255], [0,0,0])
             #self.answerBlocks[answerNum] = Answers(self.screen, answer, 100,100,100,100,[0,0,0],[0,0,0])
             #self.answerBlocks[answerNum].createAnswerBlock(self.answers)
-            self.answerBlocks[answerNum].draw()
+            #self.answerBlocks[answerNum].draw()
             answerNum += 1
+
+    def drawAll(self):
+        for answers in self.answerBlocks:
+            self.answerBlocks[answers].draw(self.screen)
+
+    def whichOneClicked(self,x,y):
+        for answer in self.answerBlocks:
+            if self.answerBlocks[answer].rect.pointInBlock(x,y):
+                return answer
+
         
 
 class Quiz():
+    """
+        Start a quiz
+        Takes in:
+            the screen it is being displayed to
+            the file source for the questions
+        Functions:
+            select a random question from the ones remaining
+            remove a question from the ones left
+            initialize a question object, and set as the current question
+            keep track of the score
 
+    """
     def __init__(self, screen, source):
         self.screen = screen
-        quiz = QuestionReader.getQuestionAndAnswers('.\data','\questions.txt')
-        self.questions = quiz[0]
-        self.answerKey = quiz[1]
+         #questions, answers = QuestionReader.getQuestionAndAnswers('.\data','\questions.txt')
+        questions = QuestionReader.getQuestionAndAnswers('.\data','\questions.txt')
+        self.questions = questions[0]
+        self.answerKey = questions[1]
         #print(self.questions)
         #print(self.answerKey)
         self.answerNum = {}
@@ -151,15 +202,26 @@ class Quiz():
         for questionName in self.questions:
             self.answerNum[questionNum] = questionName
             questionNum += 1
+        #print(self.answerNum)
+
+    def mainMenu(self):
+        pass
 
     def selectRandomQuestion(self):
-        self.currentQuestion = self.answerNum[random.randint(0,len(self.answerNum))]
+        #self.currentQuestion = self.answerNum[random.randint(0,len(self.answerNum))]
+        #print(self.currentQuestion)
+        choice = random.choice(list(self.answerNum.keys()))
+        print(choice)
+        print(self.answerNum[choice])
+        self.currentQuestion = self.answerNum[choice]
         print(self.currentQuestion)
         #return self.currentQuestion
 
     def initializeQuestion(self):
         self.currentQuestion = Question(self.screen, 'Something', self.currentQuestion, self.questions[self.currentQuestion],self.answerKey[self.currentQuestion])
         self.currentQuestion.createAnswers()
+        self.currentQuestion.drawAll()
+        text_to_screen(self.screen,self.currentQuestion.question,100,100)
 
 """
     Main Game Code
@@ -190,6 +252,7 @@ def main():
                 print(pos)
                 #answer1.rect.pointInBlock(pos[0],pos[1])
                 #answer2.rect.pointInBlock(pos[0],pos[1])
+                currentQuiz.currentQuestion.whichOneClicked(pos[0],pos[1])
 
             if event.type == pygame.QUIT:
                 pygame.quit(); sys.exit()
